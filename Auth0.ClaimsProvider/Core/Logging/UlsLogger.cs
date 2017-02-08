f3f708a5eb44ed71f14a38a72e17c7164eb9b835
phrueegsegger <philipp.rueegsegger@gmail.com>
2017-02-08 11:13:08 +0100

    deleted crypto key

---
+++ Auth0.ClaimsProvider/Core/Logging/UlsLogger.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.SharePoint.Administration;
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Auth0.ClaimsProvider.Core.Logging
+{
+    public class UlsLogger : SPDiagnosticsServiceBase
+    {
+        public static string AreaName = "Auth0";
+
+        private static UlsLogger _instance;
+
+        private static readonly object _syncLock = new object();
+
+        public static UlsLogger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncLock)
+                    {
+                        if (_instance == null)
+                            _instance = new UlsLogger();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private UlsLogger()
+            : base("Auth0 Logging Service", SPFarm.Local)
+        {
+
+        }
+
+        protected override IEnumerable<SPDiagnosticsArea> ProvideAreas()
+        {
+            return new List<SPDiagnosticsArea>
+            {
+                new SPDiagnosticsArea(AreaName, new List<SPDiagnosticsCategory> 
+                {
+                    new SPDiagnosticsCategory("ClaimsProvider", TraceSeverity.Verbose, EventSeverity.Verbose),
+                    new SPDiagnosticsCategory("ClaimsProviderErrors", TraceSeverity.Unexpected, EventSeverity.Error)
+                })
+            };
+        }
+
+        public static void Write(string message, params object[] args)
+        {
+            try
+            {
+                var category = UlsLogger.Instance.Areas[AreaName].Categories["ClaimsProvider"];
+                UlsLogger.Instance.WriteTrace(0, category, TraceSeverity.Verbose,
+                    args != null && args.Length > 0 ? String.Format(message, args) : message);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public static void WriteError(string message, params object[] args)
+        {
+            try
+            {
+                var category = UlsLogger.Instance.Areas[AreaName].Categories["ClaimsProviderErrors"];
+                UlsLogger.Instance.WriteTrace(0, category, TraceSeverity.Unexpected,
+                    args != null && args.Length > 0 ? String.Format(message, args) : message);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Auth0.ClaimsProvider/SetFarmProperty.ps1
@@ -0,0 +1,55 @@
+﻿If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )  
+{ Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell } 
+
+$extranetUrlKey = 'ExtranetWebAppUrl'; #Do not change the key!
+$extranetUrlValue = 'https://extranet.gmaare.migros.ch';
+$identityTokenIssuerName = "Auth0" #Do not change the key!
+$claimsProviderInternalName = "Auth0FederatedUsers" #Do not change the key!
+
+
+$spfFarm=Get-SPFarm;
+
+if($spfFarm.Properties.ContainsKey($extranetUrlKey)){
+    $spfFarm.Properties.Remove($extranetUrlKey);
+}
+
+$spfFarm.Properties.Add($extranetUrlKey,$extranetUrlValue);
+$spfFarm.Update();
+
+
+
+
+
+
+function Enable-ClaimsProvider($identityTokenIssuerName, $claimsProviderInternalName) {
+	
+	try {
+        Write-Host "Disabling feature..."
+        Disable-SPFeature -Identity "Auth0ClaimsProvider" -Confirm:$false -ErrorAction SilentlyContinue
+	}
+	catch [system.exception] {
+        Write-Host "Warning: Disabling the feature failed (maybe it was already disabled)"
+    }
+    
+	try {
+        Write-Host "Enabling feature..."
+        Enable-SPFeature -Identity "Auth0ClaimsProvider" -Confirm:$false -ErrorAction SilentlyContinue
+	}
+	catch [system.exception] {
+        Write-Host "Warning: Enabling the feature failed (maybe it was already enabled)"
+    }
+
+    # Update
+	Write-Host "Associating SP Trusted Identity Token Issuer (Auth0) with the Claims Provider ($claimsProviderInternalName)"
+	$spti = Get-SPTrustedIdentityTokenIssuer -identity $identityTokenIssuerName 
+	$spti.ClaimProviderName = $claimsProviderInternalName
+	$spti.Update();
+	
+    # Done.
+    Write-Host "Done. Please, go to 'SharePoint Central Admin' -> 'Security':"
+    Write-Host " 1. Under General Security section, click on 'Configure Auth0 Claims Provider'"
+    Write-Host " 2. Set the required configuration parameters"
+    Write-Host ""
+}
+
+Enable-ClaimsProvider $identityTokenIssuerName $claimsProviderInternalName
